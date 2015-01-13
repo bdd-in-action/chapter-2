56b3c9cf3238327bb0f8a63a4fa9b5e5fe12f81d
Jakub Milkiewicz <jmilkiewicz@gmail.com>
2015-01-13 19:08:59 +0100

    refactoring the test + adding extra test to validate if returned stations are immutable

--- train-timetables/src/test/java/com/bddinaction/chapter2/unit/LineIsImmutableTest.java
+++ train-timetables/src/test/java/com/bddinaction/chapter2/unit/LineIsImmutableTest.java
@@ -1,23 +1,33 @@
 package com.bddinaction.chapter2.unit;
 
 import com.bddinaction.chapter2.model.Line;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.Arrays;
-import java.util.List;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class LineIsImmutableTest {
+    private String[] inputStations = new String[]{"station 1", "station 2", "station 3"};
+    private String[] inputStationsCopy = Arrays.copyOf(inputStations, inputStations.length);
+    private Line sampleLine;
+
+    @Before
+    public void setUp() throws Exception {
+        sampleLine = Line.named("favouriteLine").departingFrom("station 0").withStations(inputStations);
+    }
 
     @Test
     public void stationsCanNotBeModifiedAfterLineCreation() throws Exception {
-        String[] stations = {"station 1", "station 2", "station 3"};
-        String[] stationsSnapshot = Arrays.copyOf(stations, stations.length);
-        Line sampleLine = Line.named("favouriteLine").departingFrom("station 0").withStations(stations);
-        stations[0] = "station 99";
+        inputStations[0] = "station 99";
 
-        assertThat(sampleLine.getStations(),is(Arrays.asList(stationsSnapshot)));
+        assertThat(sampleLine.getStations(),is(Arrays.asList(inputStationsCopy)));
+    }
+
+    @Test(expected = UnsupportedOperationException.class)
+    public void returnedLineStationCanNotBeModified() throws Exception {
+        sampleLine.getStations().add("station 99");
     }
 }
